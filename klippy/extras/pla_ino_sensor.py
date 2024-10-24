a64c6cd92bcba7e4678e73429e9e8679f31e1412
Marcus Roth <marcus.roth@plasmics.com>
2024-10-24 10:29:42 +0200

    formating

--- klippy/extras/pla_ino_sensor.py
+++ klippy/extras/pla_ino_sensor.py
@@ -17,6 +17,7 @@ from .protobuf_utils import protobuf_utils
 import time
 
 from serial.threaded import ReaderThread, Packetizer
+
 # from queue import Queue, Empty
 
 # determines the timing for all interactions with INO including reading, writing and connection (attempts)
@@ -24,8 +25,6 @@ SERIAL_TIMER = 0.1
 
 # determines the timing for heartbeat messages to the INO board
 HEARTBEAT_TIMER = 0.3
-
-
 
 
 class PlaInoSensor:
@@ -70,9 +69,10 @@ class PlaInoSensor:
 
         self.ino_controller = None
 
-
         self.gcode.register_command(
-            "INO_PID_TUNE", self.cmd_INO_PID_TUNE, desc=self.cmd_INO_PID_TUNE_help
+            "INO_PID_TUNE",
+            self.cmd_INO_PID_TUNE,
+            desc=self.cmd_INO_PID_TUNE_help
         )
         self.gcode.register_command(
             "INO_READ_PID_VALUES",
@@ -96,7 +96,6 @@ class PlaInoSensor:
         )
 
         logging.info(f"J: All Gcode commands added.")
-
 
     def make_heater_known(self, heater, config):
         """This function is called once the heater is set up - acts as a handshake between heater and sensor
@@ -135,7 +134,6 @@ class PlaInoSensor:
         logging.info("J: Klipper reports shutdown: Ino heater shutting down")
         self._handle_disconnect()
 
-
     def disconnect(self):
         """Once disconnect is called, the sensor will start shutting down.
         This includes:
@@ -148,15 +146,16 @@ class PlaInoSensor:
 
         try:
             self.ino_controller = None
-            # self.serial.close()   #TODO MR 17.10.2024: ask joes if yes or no
+            # self.serial.close()   #TODO MR 17.10.2024: ask joey if yes or no
             logging.info("Serial port closed due to disconnect.")
         except Exception as e:
             logging.error(f"J: Disconnection failed due to: {e}")
         self.serial = None
+
         try:
             self.reactor.unregister_timer(self.read_timer)
         except:
-            logging.info( "J: Reactor read timer already unregistered before disconnection." )
+            logging.info("J: Reactor read timer already unregistered before disconnection.")
         self.read_timer = None
 
         try:
@@ -186,10 +185,9 @@ class PlaInoSensor:
 
     ### INO specifics
 
-    #TODO MR 23.10.2024: function is used by pla_ino_heater.py change this!
+    # TODO MR 23.10.2024: function is used by pla_ino_heater.py change this!
     def send_temp(self):
         self.ino_controller.heat_to_target_temp(self.target_temp)
-
 
     def _sample_PLA_INO(self, eventtime):
         """This function is called infinitely by the reactor class every SERIAL_TIMER interval.
@@ -220,14 +218,13 @@ class PlaInoSensor:
         return eventtime + SERIAL_TIMER
 
     def _init_PLA_INO(self):
-        """Initializes the INO by starting a serial connection to the ino board
-        """
+        """Initializes the INO by starting a serial connection to the ino board"""
         try:
-            self.ino_controller = InoController(self,self.serial_port)
+            self.ino_controller = InoController(self, self.serial_port)
             logging.info("Connection to Ino successful.")
             self._failed_connection_attempts = 0
         except Exception as e:
-            logging.error(f"Unable to connect to Ino. This was attempt number {self._failed_connection_attempts + 1}. Exception: {e}" )
+            logging.error(f"Unable to connect to Ino. This was attempt number {self._failed_connection_attempts + 1}. Exception: {e}")
             self._failed_connection_attempts += 1
             return
 
@@ -241,14 +238,12 @@ class PlaInoSensor:
 
         logging.info("Ino read/write timers started.")
 
-        
         if self._first_connect:
             self.ino_controller.heater_off_now()
             self.ino_controller.start_pid_tuning(0)
             self.ino_controller.request_ino_reset_error()
             self.ino_controller.send_heartbeat()
 
-            
     def _run_Read(self, eventtime):
         """Readout of the incoming messages over the serial port
 
@@ -265,7 +260,6 @@ class PlaInoSensor:
         self._process_read_queue()
         return eventtime + SERIAL_TIMER
 
-
     def _run_Write(self, eventtime):
         """Write the messages that are in the queue to the serial connection
 
@@ -274,8 +268,8 @@ class PlaInoSensor:
         :return: tell reactor not to call this function any more (if not available)
         :rtype: ?
         """
-        #TODO MR:put this in a dedicated place where it belongs. This place is temporary! or check if manage heartbeat is executed, and if dont execute the rest
-        self.ino_controller.manage_heartbeat()  
+        # TODO MR:put this in a dedicated place where it belongs. This place is temporary!
+        self.ino_controller.manage_heartbeat()
 
         while not len(self.write_queue) == 0:
             text_line = self.write_queue.pop(0)
@@ -290,7 +284,6 @@ class PlaInoSensor:
 
         # logging.info("J: Write queue is empty.")
         return eventtime + SERIAL_TIMER
-
 
     def _get_extruder_for_commands(self, index, gcmd):
         """lookup of the extruder the heater and sensor belong to
@@ -311,8 +304,7 @@ class PlaInoSensor:
             extruder = self.printer.lookup_object("toolhead").get_extruder()
         return extruder
 
-
-    def _get_error_code(self, message): #TODO MR make this more elegant!
+    def _get_error_code(self, message):  # TODO MR make this more elegant!
         """extracts the error code from the numerated message
 
         :param message: message that contains the error code
@@ -367,89 +359,78 @@ class PlaInoSensor:
             error_code = "temperature_read_error"
         else:
             error_code = "unknown_error"
-            
-        return "\nnow: " + error_code + "  \npast: " + past_error_code
 
+        return "\nnow: " + error_code + "  \npast: " + past_error_code
 
     cmd_INO_PID_TUNE_help = "z.B.: INO_PID_TUNE PID=250"
     def cmd_INO_PID_TUNE(self, gcmd):
-        """custom gcode command for tuning the PID parameters of the ino board
+        """custom gcode command: start PID tuning with INO board
 
-        :param gcmd: gcode command (object) that is processed
-        :type gcmd: ?
+        :param gcmd: Temperature value to start PID tuning at
         """
-        variable = gcmd.get_float('PID', 0.)
+        variable = gcmd.get_float("PID", 0.0)
         self.ino_controller.start_pid_tuning(variable)
-
 
     cmd_INO_RESET_ERROR_help = "resets INO board error"
     def cmd_INO_RESET_ERROR(self, gcmd):
-        """custom gcode command the reset error modes in the INO board
-
-        :param gcmd: gcode command (object) that is processed
-        :type gcmd: ?
+        """custom gcode command: reset INO board error
         """
         self.ino_controller.request_ino_reset_error()
 
-
-
     cmd_INO_READ_PID_VALUES_help = "returns current ino board PID values"
     def cmd_INO_READ_PID_VALUES(self, gcmd):
-
+        """custom gcode command: requests current PID values from INO board
+        """
         self.ino_controller.request_ino_pid_values()
-
 
     cmd_INO_FIRMWARE_VERSION_help = "returns firmware version of INO board"
     def cmd_INO_FIRMWARE_VERSION(self, gcmd):
-
+        """custom gcode command: requests firmware version from INO board
+        """
         self.ino_controller.request_ino_fw_version()
-
-
 
     cmd_INO_ERROR_OUTPUT_help = "returns current error code of INO board"
     def cmd_INO_ERROR_OUTPUT(self, gcmd):
-
+        """custom gcode command: requests current and last error from INO board
+        """
         self.ino_controller.request_ino_error()
 
-
-    #TODO MR 24.10.24 add a wait function if queue is longer than 1 maybe that will print all messages?
+    # TODO MR 24.10.24 add a wait function if queue is longer than 1 maybe that will print all messages?
     def _process_read_queue(self):
         # Process any decoded lines from the device
-        while not len(self.read_queue) == 0:   
+        while not len(self.read_queue) == 0:
             first_queue_element = self.read_queue.pop(0)
             try:
-                if first_queue_element.WhichOneof('responses') == 'ino_standard_msg':    # receive standard message from ino, 
-                    self.temp = first_queue_element.ino_standard_msg.temp                # get temp from standard message
-
+                if (first_queue_element.WhichOneof("responses") == "ino_standard_msg"):  # receive standard message from ino,
+                    self.temp = (first_queue_element.ino_standard_msg.temp)  # get temp from standard message
                     logging.info(f"{timestamp()} tick:{first_queue_element.ino_standard_msg.tick}, temperature:{first_queue_element.ino_standard_msg.temp}, target_temp:{first_queue_element.ino_standard_msg.temp_target}, error_code:{first_queue_element.ino_standard_msg.error_code}, status:{first_queue_element.ino_standard_msg.status}, DC:{first_queue_element.ino_standard_msg.DC}")
 
-
-                elif first_queue_element.WhichOneof('responses') == 'ino_general_msg':
-                    if first_queue_element.ino_settings.HasField('kp'):  #check if kp is contained, only triplets of kp ki kd wil be sent
+                elif first_queue_element.WhichOneof("responses") == "ino_general_msg":
+                    if first_queue_element.ino_settings.HasField("kp"):  # check if kp is contained, only triplets of kp ki kd wil be sent
                         self.gcode.respond_info(f"Kp:{first_queue_element.ino_general_msg.kp}, Ki:{first_queue_element.ino_general_msg.ki}, Kd:{first_queue_element.ino_general_msg.kd}")
 
-                    if first_queue_element.ino_settings.HasField('pid_tune_finished'):  
+                    if first_queue_element.ino_settings.HasField("pid_tune_finished"):
                         self.gcode.respond_info(f"pid tune finished")
-                    
-                    if first_queue_element.ino_settings.HasField('general_message'): 
+
+                    if first_queue_element.ino_settings.HasField("general_message"):
                         self.gcode.respond_info(f"general_message:{first_queue_element.ino_general_msg.general_message}")
 
-                elif first_queue_element.WhichOneof('responses') == 'log_msg':
-                    if (first_queue_element.log_msg.message.startswith("s_")):
-                       logging.info(f"{timestamp()} {first_queue_element.log_msg.message}") 
+                elif first_queue_element.WhichOneof("responses") == "log_msg":
+                    if first_queue_element.log_msg.message.startswith("s_"):
+                        logging.info(f"{timestamp()} {first_queue_element.log_msg.message}")
 
-                    elif (first_queue_element.log_msg.message.startswith("error_code:")):
-                        self.gcode.respond_info( self._get_error_code(first_queue_element.log_msg.message) )
+                    elif first_queue_element.log_msg.message.startswith("error_code:"):
+                        self.gcode.respond_info(self._get_error_code(first_queue_element.log_msg.message))
 
                     else:
                         self.gcode.respond_info(f"ino: {first_queue_element.log_msg.message}")
 
                 else:
                     logging.info(f"message not recognized: {first_queue_element}")
-                
+
             except:
                 logging.info(f"\nmessage not recognized: {first_queue_element}\n")
-    
+
     def add_request_to_sendqueue(self, request):
         """Adds the request to the send queue that will be transmitted to ino board.
 
@@ -458,9 +439,7 @@ class PlaInoSensor:
         """
         packetizer = PlaSerialProtocol()
         encoded_request = packetizer.encode(request)
-        self.write_queue.append(encoded_request) #rename to send_queue
-
-           
+        self.write_queue.append(encoded_request)  # rename to send_queue
 
 
 def load_config(config):
@@ -470,13 +449,12 @@ def load_config(config):
     pheaters.add_sensor_factory("PLA_INO_SENSOR", PlaInoSensor)
 
 
-
 ####TODO LEE -> Organize the code better
-class PlaSerialProtocol():
+class PlaSerialProtocol:
 
     def __init__(self) -> None:
-        self.escape = b'\x7d'
-        self.TERMINATOR = b'\x7e'
+        self.escape = b"\x7d"
+        self.TERMINATOR = b"\x7e"
 
     def decode_framing(self, data):
         index = 0
@@ -486,13 +464,13 @@ class PlaSerialProtocol():
                 break
             if index_value < len(data) - 1:
                 xor_byte = data[index_value + 1] ^ 0x20
-                data = data[:index_value] + data[index_value + 1:]
-                data = data[:index_value] + bytes([xor_byte]) + data[index_value + 1:]
+                data = data[:index_value] + data[index_value + 1 :]
+                data = data[:index_value] + bytes([xor_byte]) + data[index_value + 1 :]
             else:
-                data = data[:index_value] + data[index_value + 1:]
+                data = data[:index_value] + data[index_value + 1 :]
             index = index_value + 1
         return data
-    
+
     def encode_framing(self, data):
         processed_array = bytearray()
         index = 0
@@ -510,11 +488,9 @@ class PlaSerialProtocol():
 
         return bytes(processed_array + self.TERMINATOR)
 
-
     def calculate_checksum(self, data):
         checksum = sum(data) & 0xFF
         return checksum
-
 
     def decode_protobuf(self, data):
         try:
@@ -523,15 +499,14 @@ class PlaSerialProtocol():
         except Exception as e:
             print(f"Error: {e}")
             return None
-        
+
         return ino_response
-    
 
     def encode(self, ino_request):
         serial_request = ino_request.SerializeToString()
 
         # TODO: Remove sequence number
-        sequence = 0         
+        sequence = 0
         serial_request = bytes([sequence]) + serial_request
 
         checksum = self.calculate_checksum(serial_request)
@@ -539,47 +514,42 @@ class PlaSerialProtocol():
 
         encoded_bytes = self.encode_framing(serial_request)
         return encoded_bytes
-    
-        
+
     def decode(self, data):
         decoded_bytes = self.decode_framing(data)
         checksum = decoded_bytes[-1]
         calculated_checksum = self.calculate_checksum(decoded_bytes[:-1])
-        
+
         if checksum != calculated_checksum:
             print(f"Checksums do not match: {checksum} != {calculated_checksum}")
             return
-        
-        return self.decode_protobuf(bytes(decoded_bytes[1:-1]))
 
+        return self.decode_protobuf(bytes(decoded_bytes[1:-1]))
 
 
 class PlaSerialHandler(Packetizer):
 
     def __init__(self) -> None:
         super().__init__()
-        self.escape = b'\x7d'
+        self.escape = b"\x7d"
         self.TERMINATOR = bytes([126])
         self.protocol_decoder = PlaSerialProtocol()
         self.queue = None
 
     def connection_made(self, transport):
         super(PlaSerialHandler, self).connection_made(transport)
-        print('port opened', transport)
-
+        print("port opened", transport)
 
     def handle_packet(self, data):
         ino_reponse = self.protocol_decoder.decode(data)
         if self.queue:
             self.queue.put(ino_reponse)
 
-
     def set_queue(self, queue):
         self.queue = queue
 
 
-
-class InoController():
+class InoController:
 
     def __init__(self, pla_obj, serial_port) -> None:
         self.pla_obj = pla_obj
@@ -604,21 +574,18 @@ class InoController():
         self.reader_thread = ReaderThread(self.serial, lambda: self.packet_handler)
         self.reader_thread.start()
 
-
     def __del__(self):
         self.close()
-
 
     def close(self):
         if self.reader_thread is None:
             return
-        
+
         print("Closing ino controller")
 
         self.reader_thread.stop()
         self.reader_thread.join()
         self.serial.close()
-
 
     def send_request_now(self, request):
         """sends the request now, and not add it to the send queue to be sent later
@@ -629,7 +596,6 @@ class InoController():
         packetizer = PlaSerialProtocol()
         encoded_request = packetizer.encode(request)
         self.reader_thread.write(encoded_request)
-
 
     def heat_to_target_temp(self, target_temp):
         """Heats ino to the specified target temperature.
@@ -645,8 +611,7 @@ class InoController():
         self.current_target_temp = target_temp
 
     def heater_off_now(self):
-        """Adds a request to the send queue to turn off the heater.
-        """
+        """Adds a request to the send queue to turn off the heater."""
         ino_request = ino_msg_pb2.user_serial_request()
         ino_request.set_settings.target_temperature = 0
         self.send_request_now(ino_request)
@@ -666,12 +631,12 @@ class InoController():
         """
         if time.time() - self.last_heartbeat > HEARTBEAT_TIMER:
             self.send_heartbeat()
-            self.last_heartbeat = time.time() 
+            self.last_heartbeat = time.time()
 
     def start_pid_tuning(self, target_temp):
         """send command to ino board to start PID tuning
         target_temp: the target temperature to do the PID tuning at
-        """  
+        """
         ino_request = ino_msg_pb2.user_serial_request()
         ino_request.set_settings.pid_target_temperature = target_temp
         self.pla_obj.add_request_to_sendqueue(ino_request)
@@ -701,8 +666,7 @@ class InoController():
         self.pla_obj.add_request_to_sendqueue(ino_request)
 
     def request_ino_reset_error(self):
-        """request, the ino board to reset its errors
-        """
+        """request, the ino board to reset its errors"""
         ino_request = ino_msg_pb2.user_serial_request()
         ino_request.pla_cmd.command = ino_msg_pb2.clear_errors
         self.pla_obj.add_request_to_sendqueue(ino_request)
@@ -717,7 +681,6 @@ class InoController():
 
 
 def timestamp():
-    """Returns a timestamp in the format HH:MM:SS.ms dd.mm.yy
-    """
-    time_and_date = time.strftime("%H:%M:%S." + str(time.time()).split('.')[1][:4] + " %d.%m.%y", time.localtime())
+    """Returns a timestamp in the format HH:MM:SS.ms dd.mm.yy"""
+    time_and_date = time.strftime("%H:%M:%S." + str(time.time()).split(".")[1][:4] + " %d.%m.%y", time.localtime())
     return time_and_date
